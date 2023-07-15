2b25872f7816f1ad33bc041e50b49274af0a1d6b
lingfengjgf <lingfengjgf@163.com>
2023-07-15 15:09:34 +0800

    yargs 构建脚手架

--- lingfeng-cli/bin/index.js
+++ lingfeng-cli/bin/index.js
@@ -1,34 +1,61 @@
 #!/usr/bin/env node
 
-const lib = require("lingfeng-cli-lib");
-// 注册命令 icli init
-const argv = require("process").argv;
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const pkg = require("../package.json");
 
-// 命令
-const command = argv[2];
-
-// 参数
-const options = argv.slice(3);
-
-if (options.length > 1) {
-  let [option, param] = options;
-  option = option.replace("--", "");
-
-  if (command) {
-    if (lib[command]) {
-      lib[command]({ option, param });
-    } else {
-      console.log("无效的命令");
+const arg = hideBin(process.argv);
+const cli = yargs(arg);
+const argv = process.argv.slice(2);
+const context = {
+  icliVersion: pkg.version,
+};
+cli
+  .usage("Usage: icli <command> [options]")
+  .demandCommand(
+    1,
+    "A command is required. Pass --help to see all available commands and options."
+  )
+  .strict()
+  .recommendCommands()
+  .fail((err, msg) => {
+    console.log(err);
+  })
+  .alias("h", "help")
+  .alias("v", "version")
+  .wrap(cli.terminalWidth())
+  .epilogue("Yuor own footer description")
+  .options({
+    debug: {
+      type: "boolean",
+      describe: "开启调试模式",
+      alias: "d",
+    },
+  })
+  .option("registry", {
+    type: "string",
+    describe: "定义全局地址",
+    alias: "r",
+  })
+  .group(["debug"], "开发选项:")
+  .command(
+    "init [name]",
+    "初始化一个项目",
+    (yargs) => {
+      yargs.option("name", {
+        type: "string",
+        describe: "项目名称",
+      });
+    },
+    (argv) => {
+      console.log(argv);
     }
-  } else {
-    console.log("请输入命令");
-  }
-}
-
-// 实现参数解析 --version 和 init --name
-if (command.startsWith("--") || command.startsWith("-")) {
-  const globalOption = command.replace(/--|-/g, "");
-  if (["version", "V"].includes(globalOption)) {
-    console.log(require("../package.json").version);
-  }
-}
+  )
+  .command({
+    command: "list",
+    aliases: ["ls", "la", "ll"],
+    handler: (argv) => {
+      console.log(argv);
+    },
+  })
+  .parse(argv, context);
